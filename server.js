0b70746945d68dea20b947486ab312573876661e
Memija <anelmemija@gmail.com>
2020-02-16 17:24:54 +0100

    Redirect from HTTP to HTTPS.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const compression = require("compression");
 const express = require("express");
+const hsts = require('hsts');
 
 var app = express();
 
 app.use(compression());
 app.use(express.static(__dirname + '/dist'));
+app.use(hsts({
+    maxAge: 31536000,
+    includeSubDomains: true,
+    preload: true
+}));
+http.get('*', function (req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+});
 app.get('*.js', function (req, res, next) {
     req.url = req.url + '.gz';
     res.set('Content-Encoding', 'gzip');
